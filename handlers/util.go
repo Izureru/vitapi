2bd6472506a7818d31c869835b7c66255d8e3f78
Israel <israel.lewis@mnscorp.net>
2015-09-18 17:03:32 +0100

    set the key

--- handlers/util.go
+++ handlers/util.go
@@ -30,7 +30,7 @@ func HomeHandler(rw http.ResponseWriter, r *http.Request) {
 	log.Println("Home Handler Called")
 	rw.Header().Set("Access-Control-Allow-Origin", "*")
 	rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	rw.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, simplyactiveapikey")
+	rw.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, schnapikey")
 	rw.Header().Set("Content-Type", "application/json")
 	rw.WriteHeader(http.StatusOK)
 }
